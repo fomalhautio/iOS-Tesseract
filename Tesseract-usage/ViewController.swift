434fdca560b0048b3d3f2203e379e1ae3e582f1b
Abdulmomen Kadum عبدالمؤمن كاظم <a.bluemix@gmail.com>
2016-07-13 00:14:14 +0300

    first commit

--- Tesseract-usage/ViewController.swift
+++ Tesseract-usage/ViewController.swift
@@ -7,17 +7,56 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
 
+    
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var ocrSampleImage: UIImageView!
+    @IBOutlet weak var orcText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let tesseract: G8Tesseract = G8Tesseract(language: "eng")
+        tesseract.engineMode = .TesseractOnly
+        //tesseract.pageSegmentationMode = .Auto
+        tesseract.charWhitelist = "@.(){}/\\!*&#0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //tesseract.charWhitelist = "0123456789";
+        //tesseract.charBlacklist = "@.(){}/\\!*&#abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        tesseract.maximumRecognitionTime = 10.0
+        
+        tesseract.delegate = self
+        
+        
+        let ocrSample = UIImage(named: "Image5")
+        tesseract.image = ocrSample?.fixOrientation().g8_blackAndWhite()
+        
+        tesseract.recognize()
+
+        let recognizedText = tesseract.recognizedText
+        
+        print("recognizedText: \(recognizedText)")
+        
+        ocrSampleImage.image = tesseract.image
+        orcText.text = recognizedText
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func progressImageRecognitionForTesseract(tesseract: G8Tesseract!) {
+        print("\(tesseract.progress)")
     }
 
 
