bc96753c5ea97064c2667f2e232428764ebdfb78
Abdulmomen Kadum عبدالمؤمن كاظم <a.bluemix@gmail.com>
2016-07-12 23:12:57 +0300

    Initial Commit

---
+++ Tesseract-usage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tesseract-usage
+//
+//  Created by blueMix on 7/12/16.
+//  Copyright © 2016 Fomalhaut.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
